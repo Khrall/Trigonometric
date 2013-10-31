a05db2f9fe1c291fd15471ca2b1d7053e3ddda35
Raymi T. Eldby <raymitoro@gmail.com>
2013-11-01 00:42:17 +0100

    Started project Trigonometric

---
+++ src/com/elddev/trigonometric/ListIdentities.java
@@ -0,0 +1,18 @@
+package com.elddev.trigonometric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ListIdentities extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_list_identities);
+
+		
+	}
+
+}
