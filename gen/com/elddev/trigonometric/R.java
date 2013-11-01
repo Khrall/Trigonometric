84902d1484c6e9ea67acc6f8321a14128cbf8428
Raymi Eldby <raymitoro@gmail.com>
2013-11-01 13:16:19 +0100

    Added a JSON-string for holding identities, as well as support for retrieving and displaying the data stored in the app

--- gen/com/elddev/trigonometric/R.java
+++ gen/com/elddev/trigonometric/R.java
@@ -19,29 +19,39 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int background_color=0x7f040000;
-        public static final int black_overlay=0x7f040002;
-        public static final int header_color=0x7f040001;
-        public static final int shadow_color=0x7f040003;
+        public static final int background_color=0x7f050000;
+        public static final int black_overlay=0x7f050002;
+        public static final int dark_on_background_bg=0x7f050006;
+        public static final int dark_on_background_font_color=0x7f050007;
+        public static final int header_color=0x7f050001;
+        public static final int on_white_box_font_color=0x7f050005;
+        public static final int shadow_color=0x7f050003;
+        public static final int white_box_color=0x7f050004;
     }
     public static final class drawable {
-        public static final int header_bg=0x7f020000;
-        public static final int header_box=0x7f020001;
-        public static final int header_shadow=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int equation_bg=0x7f020000;
+        public static final int equation_box=0x7f020001;
+        public static final int equation_shadow=0x7f020002;
+        public static final int header_bg=0x7f020003;
+        public static final int header_box=0x7f020004;
+        public static final int header_shadow=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int TextView01=0x7f070002;
-        public static final int TextView02=0x7f070001;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070003;
+        public static final int identityList=0x7f080001;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_list_identities=0x7f030000;
     }
+    public static final class raw {
+        public static final int identities=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int identities_header_text=0x7f050000;
+        public static final int app_name=0x7f060001;
+        public static final int identities_description_text=0x7f060002;
+        public static final int identities_header_text=0x7f060000;
     }
     public static final class style {
         /** 
@@ -68,15 +78,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060004;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070002;
     }
     public static final class styleable {
         /** 
--- src/com/elddev/trigonometric/ListIdentities.java
+++ src/com/elddev/trigonometric/ListIdentities.java
@@ -1,18 +1,102 @@
 package com.elddev.trigonometric;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ListIdentities extends Activity {
+
+private ArrayList<TrigIdentity> identities; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_list_identities);
-
+		
+		LinearLayout identityListLayout = (LinearLayout) findViewById(R.id.identityList);
+		identities = new ArrayList<TrigIdentity>();
+		
+		JSONObject identityData = null;
+		try { 
+			identityData = getIdentityData();
+			identities = generateListFromData( identityData );
+			
+			for(TrigIdentity t : identities) {
+				FrameLayout layout = new FrameLayout(this);
+				layout.setBackgroundResource(R.drawable.equation_box);
+				
+				TextView text = new TextView(this);
+				text.setText( t.toTextViewString() );
+				text.setPadding(0, 15, 0, 15);
+				text.setGravity(Gravity.CENTER_HORIZONTAL);
+				text.setTextColor(getResources().getColor(R.color.on_white_box_font_color));
+				text.setTextSize(18);
+				text.setLineSpacing(10, 1);
+				
+				layout.addView(text );
+				identityListLayout.addView( layout );
+			}
+		} catch (JSONException e) { e.printStackTrace(); }
+		
+		
 		
 	}
 
+	private ArrayList<TrigIdentity> generateListFromData(JSONObject data) throws JSONException {
+		ArrayList<TrigIdentity> identities = new ArrayList<TrigIdentity>();
+		
+		JSONArray identitiesData = data.getJSONArray("identities");
+		
+		for(int i = 0; i < identitiesData.length(); i++) {
+			JSONObject identityData = identitiesData.getJSONObject(i);
+			TrigIdentity t = new TrigIdentity(identityData.getString("name"));
+			
+			JSONArray equationsData = identityData.getJSONArray("equations");
+			for(int j = 0; j < equationsData.length(); j++) {
+				t.addEq( equationsData.getString(j) );
+			}
+			
+			identities.add(t);
+		}
+		
+		return identities;
+	}
+
+	private JSONObject getIdentityData() throws JSONException {
+		String result = "No result";
+		InputStream content = getResources().openRawResource(R.raw.identities);
+		
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(content,"iso-8859-1"),8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			content.close();
+  
+			result=sb.toString();
+      	} catch(Exception e){
+      		Log.e("log_tag", "Error converting result "+e.toString());
+      	}
+		
+		return new JSONObject(result);
+	}
+	
 }
